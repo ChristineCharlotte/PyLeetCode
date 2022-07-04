36774a7fe0ed143f504ed8c0961eff04415bcf21
Bingo <faaa@live.com>
2022-07-04 19:09:24 +0800

    update

--- 102 binary-tree-level-order-traversal.py
+++ 102 binary-tree-level-order-traversal.py
@@ -2,7 +2,10 @@
 @Time    : 2022/7/4 17:01
 @Author  : 刘俊 jun.liu@deepfinance.com
 @File    : 102 binary-tree-level-order-traversal.py
+
+二叉树的层序遍历
 """
+
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -12,7 +15,18 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-
-        def level_traversal(root: TreeNode) -> List[List[int]]:
-            arr = []
-
+        ret = []
+        if root is None:
+            return []
+        arr = [root]
+        while len(arr) > 0:
+            ret.append([])
+            for i in range(len(arr)):
+                node = arr[0]
+                ret[len(ret) - 1].append(node.val)
+                if node.left is not None:
+                    arr.append(node.left)
+                if node.right is not None:
+                    arr.append(node.right)
+                del arr[0]
+        return ret
---
+++ test.py
@@ -0,0 +1,11 @@
+"""
+@Time    : 2022/7/4 19:06
+@Author  : 刘俊 jun.liu@deepfinance.com
+@File    : test.py
+"""
+
+l = [1, 2, 3, 4]
+
+for i in l:
+    print(l)
+    del l[len(l)-1]
